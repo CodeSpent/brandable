b114adb179c5f77386c9b0b8dcae3da67c882fda
Patrick Hanford <hanfordpatrick@gmail.com>
2020-01-22 21:39:28 -0500

    fix(header): add margin to header at lg breakpoint
    Adds a top margin to header on larger displays to keep consistent spacing.

--- src/components/header.js
+++ src/components/header.js
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Header() {
   return (
-    <div className="px-2 py-1 text-center">
+    <div className="px-2 py-1 text-center lg:mt-12">
       <h1 className="text-6xl font-black">Brandable</h1>
-      <h2 className="my-5 text-2xl font-light">
+      <h2 className="mb-5 text-2xl font-light">
         Check the{" "}
         <span className="italic font-bold text-green">brandability score</span>{" "}
         for your product name! 🚀
